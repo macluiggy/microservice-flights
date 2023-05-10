e6071382b784813cae739237b3e3251197fba62e
macluiggy <luiggy.macias@imagineapps.co>
2023-05-09 19:27:00 -0500

    flight controller

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -12,46 +12,43 @@ import {
 } from '@nestjs/common';
 import { FlightService } from './flight.service';
 import { FlightDTO } from './dto/flight.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { FlightMSG } from 'src/common/constants';
 
 @Controller('api/v1/flight')
 export class FlightController {
   constructor(
-    private readonly flightService: FlightService,
-    // private readonly passengerService: PassengerService,
+    private readonly flightService: FlightService, // private readonly passengerService: PassengerService,
   ) {}
-  @Post()
-  async create(@Body() flightDto: FlightDTO) {
-    return await this.flightService.create(flightDto);
+  @MessagePattern(FlightMSG.CREATE)
+  create(@Payload() flightDto: FlightDTO) {
+    return this.flightService.create(flightDto);
   }
 
-  @Get()
-  async findAll() {
-    return await this.flightService.findAll();
+  @MessagePattern(FlightMSG.FIND_ALL)
+  findAll() {
+    return this.flightService.findAll();
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.flightService.findOne(id);
+  @MessagePattern(FlightMSG.FIND_ONE)
+  findOne(@Payload() id: string) {
+    return this.flightService.findOne(id);
   }
 
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() flightDto: FlightDTO) {
-    return await this.flightService.update(id, flightDto);
+  @MessagePattern(FlightMSG.UPDATE)
+  update(@Payload() { id, flightDto }: { id: string; flightDto: FlightDTO }) {
+    return this.flightService.update(id, flightDto);
   }
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    return await this.flightService.delete(id);
+  @MessagePattern(FlightMSG.DELETE)
+  delete(@Payload() id: string) {
+    return this.flightService.delete(id);
   }
 
-  // @Post(':id/passenger/:passengerId')
-  // async addPassenger(
-  //   @Param() { id, passengerId }: { id: string; passengerId: string },
-  // ) {
-  //   const passenger = await this.passengerService.findOne(passengerId);
-  //   if (!passenger) {
-  //     throw new HttpException('Passenger not found', HttpStatus.NOT_FOUND);
-  //   }
-  //   return await this.flightService.addPassenger(id, passengerId);
-  // }
+  @MessagePattern(FlightMSG.ADD_PASSENGER)
+  addPassenger(
+    @Payload() { id, passengerId }: { id: string; passengerId: string },
+  ) {
+    return this.flightService.addPassenger(id, passengerId);
+  }
 }
