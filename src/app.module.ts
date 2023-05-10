8e215bbe75a8f9db5c9f50aba5c8cfed7b912f1a
macluiggy <luiggy.macias@imagineapps.co>
2023-05-09 19:21:44 -0500

    config flights

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FlightModule } from './flight/flight.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    FlightModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
